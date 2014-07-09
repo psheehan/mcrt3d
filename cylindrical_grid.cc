bb39faceaa7f09b30565f4d3039e1b52c169547b
Patrick Sheehan <psheehan287@gmail.com>
2014-07-09 12:44:29 -0700

    Made a number of changes to CylindricalGrid: - Testing of whether a photon is in the grid is now done using the cell   index of the photon. - Started using Photon->rad, theta, phi to test for the distance to the   next wall. This is because of roundoff error problems. - The routine for finding the cell a photon is in now uses Photon->rad,   phi, theta and will adjust their values to be exactly the values of   the walls if the photon is supposed to be on a wall.

--- cylindrical_grid.cc
+++ cylindrical_grid.cc
@@ -15,60 +15,54 @@ struct CylindricalGrid : public Grid {
 
 double CylindricalGrid::next_wall_distance(Photon *P) {
 
-    double r = sqrt(P->r[0]*P->r[0]+P->r[1]*P->r[1]);
+    //double r = sqrt(P->r[0]*P->r[0]+P->r[1]*P->r[1]);
+    double r = P->rad;
 
     // Calculate the distance to the intersection with the next radial wall.
     
     double a = P->n[0]*P->n[0]+P->n[1]*P->n[1];
     double b = P->r[0]*P->n[0]+P->r[1]*P->n[1];
 
-    int l = P->l[0]-1;
-    if (l < 0) l = 0;
-    int u = P->l[0]+2;
-    if (u >= nw1) u = nw1-1;
-    
     double s = HUGE_VAL;
-    for (int i=l; i <= u; i++) {
-        double c = r*r - w1[i]*w1[i];
-        double d = b*b - a*c;
+    for (int i=P->l[0]; i <= P->l[0]+1; i++) {
+        if (r != w1[i]) {
+            double c = r*r - w1[i]*w1[i];
+            double d = b*b - a*c;
 
-        if (d >= 0) {
-            double sr1 = (-b + sqrt(d))/a;
-            double sr2 = (-b - sqrt(d))/a;
+            if (d >= 0) {
+                double sr1 = (-b + sqrt(d))/a;
+                double sr2 = (-b - sqrt(d))/a;
 
-            if ((sr1 < s) && (sr1 > 1.0e-6*dw1[i])) s = sr1;
-            if ((sr2 < s) && (sr2 > 1.0e-6*dw1[i])) s = sr2;
+                if ((sr1 < s) && (sr1 > 0)) s = sr1;
+                if ((sr2 < s) && (sr2 > 0)) s = sr2;
+            }
         }
     }
 
     // Calculate the distance to the intersection with the next phi wall.
     
     if (nw2 != 2) {
-        l = P->l[1]-1;
-        u = P->l[1]+2;
-        
-        for (int i=l; i <= u; i++) {
-            int index = (i+(nw2-1))%(nw2-1);
-            double c = P->r[0]*sin(w2[index])-P->r[1]*cos(w2[index]);
-            double d = P->n[0]*sin(w2[index])-P->n[1]*cos(w2[index]);
+        //double phi = fmod(atan2(P->r[1],P->r[0])+2*pi,2*pi);
+        double phi = P->phi;
 
-            double sp = -c/d;
+        for (int i=P->l[1]; i <= P->l[1]+1; i++) {
+            if (phi != w2[i]) {
+                double c = P->r[0]*sin(w2[i])-P->r[1]*cos(w2[i]);
+                double d = P->n[0]*sin(w2[i])-P->n[1]*cos(w2[i]);
 
-            if ((sp < s) && (sp > 1.0e-6*r*dw2[index])) s = sp;
+                double sp = -c/d;
+
+                if ((sp < s) && (sp > 0)) s = sp;
+            }
         }
     }
 
     // Calculate the distance to intersection with the nearest z wall.
     
-    l = P->l[2]-1;
-    if (l < 0) l = 0;
-    u = P->l[2]+2;
-    if (u >= nw3) u = nw3-1;
-    
-    for (int i=l; i <= u; i++) {
-        double sz = (w3[i] - P->r[2])*P->invn[2];
-        if ((sz < s) && (sz > 1.0e-6*dw3[i])) s = sz;
-    }
+    double sz1 = (w1[P->l[2]] - P->r[2])*P->invn[2];
+    if ((sz1 < s) && (sz1 > 0)) s = sz1;
+    double sz2 = (w1[P->l[2]+1] - P->r[2])*P->invn[2];
+    if ((sz2 < s) && (sz2 > 0)) s = sz2;
     
     return s;
 }
@@ -78,17 +72,23 @@ double CylindricalGrid::next_wall_distance(Photon *P) {
 Vector<int, 3> CylindricalGrid::photon_loc(Photon *P, bool verbose) {
     Vector<int, 3> l;
 
-    // Find the location in the radial grid.
+    /* Find the location in the radial grid. */
     
     double pi = 3.14159265;
-    double r = sqrt(P->r[0]*P->r[0]+P->r[1]*P->r[1]);
-    double phi = fmod(atan2(P->r[1],P->r[0])+2*pi,2*pi);
+    //double r = sqrt(P->r[0]*P->r[0]+P->r[1]*P->r[1]);
+    //double phi = fmod(atan2(P->r[1],P->r[0])+2*pi,2*pi);
+    P->rad = sqrt(P->r[0]*P->r[0]+P->r[1]*P->r[1]);
+    P->phi = fmod(atan2(P->r[1],P->r[0])+2*pi,2*pi);
+    double r = P->rad;
+    double phi = P->phi;
 
     double gnx = cos(phi);
     double gny = sin(phi);
     
+    /* Determine which cell the photon is currently in. */
+
     if (r >= w1[nw1-1])
-        l[0] = nw1-1;
+        l[0] = n1-1;
     else if (r <= w1[0])
         l[0] = 0;
     else {
@@ -97,16 +97,28 @@ Vector<int, 3> CylindricalGrid::photon_loc(Photon *P, bool verbose) {
         else {
             int lower = P->l[0]-1;
             if (lower < 0) lower = 0;
-            int upper = P->l[0]+2;
-            if (upper > nw1-1) upper = nw1-1;
+            int upper = P->l[0]+1;
+            if (upper > n1-1) upper = n1;
             
             l[0] = find_in_arr(r,w1,lower,upper);
         }
     }
+
+    /* Because of floating point errors it may be the case that the photon 
+     * should be on the wall exactly, but is not exactly on the wall. We
+     * need to put the photon exactly on the wall. */
+
+    if (equal(r,w1[l[0]],1.0e-6))
+        r = w1[l[0]];
+    else if (equal(r,w1[l[0]+1],1.0e-6))
+        r = w1[l[0]+1];
+
+    /* Finally, update which cell the photon is in based on the direction it
+     * is going. */
     
-    if ((equal(r,w1[l[0]],1.0e-6)) && (P->n[0]*gnx+P->n[1]*gny <= 0))
+    if ((r == w1[l[0]]) && (P->n[0]*gnx+P->n[1]*gny <= 0))
         l[0] -= 1;
-    else if ((equal(r,w1[l[0]+1],1.0e-6)) && (P->n[0]*gnx+P->n[1]*gny >= 0))
+    else if ((r == w1[l[0]+1]) && (P->n[0]*gnx+P->n[1]*gny >= 0))
         l[0] += 1;
 
     // Find the location in the phi grid.
@@ -114,37 +126,51 @@ Vector<int, 3> CylindricalGrid::photon_loc(Photon *P, bool verbose) {
     if (nw2 == 2)
         l[1] = 0;
     else {
-        if (phi >= w2[nw2-1])
-            l[1] = 0;
-        else if (phi <= w2[0])
-            l[1] = nw2-2;
-        else {
-            if (P->l[1] == -1)
-                l[1] = find_in_arr(phi,w2,nw2);
-            else {
-                int lower = P->l[1]-1;
-                int upper = P->l[1]+2;
-                
-                l[1] = find_in_periodic_arr(phi,w2,nw3,lower,upper);
-            }
-            if (l[1] == nw2-1) l[1] = 0;
-        }
+        if (P->l[1] == -1)
+            l[1] = find_in_arr(phi,w2,nw2);
+        else
+            l[1] = find_in_periodic_arr(phi,w2,n2,P->l[1]-1,P->l[1]+1);
+
+        /* Check whether the photon is supposed to be exactly on the cell
+         * wall. Floating point errors may keep it from being exactly on the
+         * wall, and we need to fix that. */
+
+        if (equal(phi,w2[l[1]],1.0e-6))
+            phi = w2[l[1]];
+        else if (equal(phi,w2[l[1]+1],1.0e-6))
+            phi = w2[l[1]+1];
+
+        /* Update which cell the photon is in depending on the 
+         * direction it is going. */
 
         double gnx = -sin(phi);
         double gny = cos(phi);
         
-        if ((equal(phi,w2[l[1]],1.0e-3)) && (P->n[0]*gnx+P->n[1]*gny <= 0))
+        if ((phi == w2[l[1]]) && (P->n[0]*gnx+P->n[1]*gny <= 0))
             l[1] -= 1;
-        else if ((equal(phi,w2[l[1]+1],1.0e-3)) && 
-                (P->n[0]*gnx+P->n[1]*gny >= 0))
+        else if ((phi == w2[l[1]+1]) && (P->n[0]*gnx+P->n[1]*gny >= 0))
             l[1] += 1;
-        l[1] = (l[1]+nw2-1)%(nw2-1);
+        l[1] = (l[1]+n2)%(n2);
+
+        /* Finally, if you are at phi = 0, but going towards negative phi, 
+         * you should set phi = 2*pi. */
+
+        if ((phi == 0) && (l[1] == n2-1))
+            phi = w2[l[1]+1];
     }
+
+    /* Since we may have updated r and phi to be exactly on the grid cell
+     * walls, change the photon position slightly to reflect this. */
+
+    P->r[0] = r * cos(phi);
+    P->r[1] = r * sin(phi);
+    P->rad = r;
+    P->phi = phi;
 
     // Find the location in the z grid.
     
     if (P->r[2] >= w3[nw3-1])
-        l[2] = nw3-1;
+        l[2] = n3-1;
     else if (P->r[2] <= w3[0])
         l[2] = 0;
     else {
@@ -153,16 +179,16 @@ Vector<int, 3> CylindricalGrid::photon_loc(Photon *P, bool verbose) {
         else {
             int lower = P->l[2]-1;
             if (lower < 0) lower = 0;
-            int upper = P->l[2]+2;
+            int upper = P->l[2]+1;
             if (upper > nw3-1) upper = nw3-1;
             
             l[2] = find_in_arr(P->r[2],w3,lower,upper);
         }
     }
     
-    if ((equal(P->r[2],w3[l[2]],1.0e-6)) && (P->n[2] <= 0))
+    if ((P->r[2] == w3[l[2]]) && (P->n[2] <= 0))
         l[2] -= 1;
-    else if ((equal(P->r[2],w3[l[2]+1],1.0e-6)) && (P->n[2] >= 0))
+    else if ((P->r[2] == w3[l[2]+1]) && (P->n[2] >= 0))
         l[2] += 1;
     
     return l;
@@ -172,14 +198,7 @@ Vector<int, 3> CylindricalGrid::photon_loc(Photon *P, bool verbose) {
 
 bool CylindricalGrid::in_grid(Photon *P) {
 
-    double r = sqrt(P->r[0]*P->r[0]+P->r[1]*P->r[1]);
-
-    if ((r >= w1[nw1-1]) || (P->r[2] >= w3[nw3-1]) || 
-            (equal(r,w1[nw1-1],1.0e-6)) || (equal(P->r[2],w3[nw3-1],1.0e-6)))
-        return false;
-    else if ((r <= w1[0]) || (equal(r,w1[0],1.0e-6)))
-        return false;
-    else if ((P->r[2] <= w3[0]) || (equal(P->r[2],w3[0],1.0e-6)))
+    if ((P->l[0] >= n1) || (P->l[0] < 0) || (P->l[2] >= n3) || (P->l[2] < 0))
         return false;
     else
         return true;
