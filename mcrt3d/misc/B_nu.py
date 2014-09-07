377613924798ae4c9db358b1e388e479e3fe25e3
Patrick Sheehan <psheehan287@gmail.com>
2014-09-06 17:17:18 -0700

    Told numpy to ignore the overflow warnings when I run B_nu or dB_nu.

--- mcrt3d/misc/B_nu.py
+++ mcrt3d/misc/B_nu.py
@@ -1,6 +1,8 @@
+import numpy
 from numpy import exp
 from ..constants.physics import h, c, k
 
 def B_nu(nu,T):
     
+    numpy.seterr(over="ignore")
     return (2*h*nu**3/c**2)/(exp(h*nu/(k*T))-1.0)
--- mcrt3d/misc/dB_nu.py
+++ mcrt3d/misc/dB_nu.py
@@ -1,7 +1,9 @@
+import numpy
 from numpy import exp
 from ..constants.physics import h, c, k
 
 def dB_nu(nu,T):
     
+    numpy.seterr(over="ignore")
     return (-2*h**2*nu**4/(c**2*k*T**2))/(exp(h*nu/(k*T))-1)/ \
             (1.-exp(-h*nu/(k*T)))
