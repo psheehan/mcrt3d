b326e6a68c95e6d7c9aa1d6f9d0dcf2a4980ea63
Patrick Sheehan <psheehan287@gmail.com>
2018-03-20 15:33:23 -0500

    Updated the run.py file to use some of the new conventions.

--- examples/run.py
+++ examples/run.py
@@ -19,7 +19,6 @@ spectra = SetupSpectra()
 # Now give that all to C.
 
 M = MCRT(G, Q)
-C = Camera(G, Q)
 
 t1 = time()
 M.run_thermal_mc()
@@ -27,10 +26,10 @@ t2 = time()
 print(t2-t1)
 
 for i in range(images.size):
-    C.make_image(images[i])
+    M.run_image(images[i])
 
 for i in range(spectra.size):
-    C.make_image(spectra[i])
+    M.run_image(spectra[i])
 
 for i in range(9):
     plt.imshow(G.temperature[0][:,:,i],origin="lower",interpolation="nearest", \
@@ -41,6 +40,6 @@ for i in range(9):
 plt.imshow(images[0].intensity[:,:,0],origin="lower",interpolation="none")
 plt.show()
 
-plt.loglog(c_l / spectra[0].nu, spectra[0].intensity[0,0,:])
-plt.axis([1e-5,1e0,1e-19,1e-9])
+plt.loglog(c_l / spectra[0].nu*1.0e4, spectra[0].intensity[0,0,:])
+plt.axis([1e-1,1e4,1e-19,1e-9])
 plt.show()
