d2be442b22a88200218343ddcec9bb0d3e2184ab
Patrick Sheehan <psheehan287@gmail.com>
2018-11-12 21:00:37 -0500

    Deleted old test files and updated test_cartesian.py to be an up-to-date test of the code.

--- test/test_cartesian.py
+++ test/test_cartesian.py
@@ -1,56 +1,207 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from numpy import loadtxt, zeros, ones, arange
-from hyperion.model import Model
-from hyperion.model import ModelOutput
-from hyperion.dust import IsotropicDust
-from hyperion.util.constants import *
+from pdspy.constants.astronomy import AU, pc, Jy
+import pdspy.modeling as modeling
+import pdspy.dust as dust
 import matplotlib.pyplot as plt
+from numpy import array, arange, pi, zeros, logspace
+import numpy
 
-m = Model()
+from mcrt3d import MCRT, Params
+from mcrt3d.grid import CartesianGrid
+from mcrt3d.dust import Dust
+from mcrt3d.sources import Star
+from mcrt3d.camera import Image, Spectrum
+from mcrt3d.constants.astronomy import M_sun, R_sun, AU
+from mcrt3d.constants.physics import c
+from time import time
 
-data = loadtxt('../dustkappa_yso.inp', skiprows=2)
-nu = c / (data[:,0].copy()[::-1] * 1.0e-4)
-kabs = data[:,1].copy()[::-1]
-ksca = data[:,2].copy()[::-1]
-chi = kabs + ksca
-albedo = ksca / chi
+################################################################################
+#
+# Run the model with RADMC3D
+#
+################################################################################
 
-d = IsotropicDust(nu, albedo, chi)
+m = modeling.Model()
+
+# Read in the opacities.
+
+data = numpy.loadtxt('../examples/dustkappa_yso.inp', skiprows=2)
+
+lam = data[:,0].copy() * 1.0e-4
+kabs = data[:,1].copy()
+ksca = data[:,2].copy()
+
+d = dust.Dust()
+d.set_properties(lam, kabs, ksca)
+
+# Set up the grid.
 
 nx = 10
 ny = 10
 nz = 10
 
-x = (arange(nx)-(float(nx)-1)/2)*au/1
-y = (arange(ny)-(float(ny)-1)/2)*au/1
-z = (arange(nz)-(float(nz)-1)/2)*au/1
+x = (numpy.arange(nx)-(float(nx)-1)/2)
+y = (numpy.arange(ny)-(float(ny)-1)/2)
+z = (numpy.arange(nz)-(float(nz)-1)/2)
 
-m.set_cartesian_grid(x, y, z)
+m.grid.set_cartesian_grid(x, y, z)
 
-dens = zeros((nx-1,ny-1,nz-1)) + 1.0e-17
+# Set up the wavelength grid.
 
-m.add_density_grid(dens, d)
+m.grid.lam = lam * 1.0e4
 
-source = m.add_spherical_source()
-source.luminosity = lsun
-source.radius = rsun
-source.temperature = 4000.
+# Set the density.
 
-m.set_n_photons(initial=1000000, imaging=0)
-m.set_convergence(True, percentile=99., absolute=2., relative=1.02)
+dens = numpy.zeros((nx-1,ny-1,nz-1)) + 1.0e-16
 
-m.write("test_cartesian.rtin")
+m.grid.add_density(dens, d)
 
-m.run("test_cartesian.rtout", mpi=False)
+# Add a star.
 
-n = ModelOutput('test_cartesian.rtout')
+source = modeling.Star(mass=0.5, luminosity=0.22658222, temperature=4000.)
 
-grid = n.get_quantities()
+m.grid.add_star(source)
 
-temp = grid.quantities['temperature'][0]
+# Run the thermal simulation.
 
-plt.imshow(temp[:,4,:],origin="lower",interpolation="nearest", \
-        vmin=temp.min(),vmax=temp.max())
-plt.colorbar()
+t1 = time()
+m.run_thermal(code="radmc3d", nphot=1e6, verbose=False)
+t2 = time()
+print(t2-t1)
+
+# Run the image.
+
+m.run_image(name="image", nphot=1e5, npix=256, pixelsize=0.1, lam="1300", \
+        phi=45, incl=45, code="radmc3d", dpc=1, verbose=False)
+
+# Run the SED.
+
+m.set_camera_wavelength(numpy.logspace(-1,4,200))
+
+m.run_sed(name="SED", nphot=1e4, loadlambda=True, incl=0, pa=0, \
+        dpc=1, code="radmc3d", camera_scatsrc_allfreq=True, \
+        verbose=False)
+
+################################################################################
+#
+# Run the model with MCRT3D
+#
+################################################################################
+
+# Create a model class.
+
+model = MCRT()
+
+# Set up the dust.
+
+dust = Dust(filename="../examples/dustkappa_yso.inp", radmc3d=True)
+
+# Set up the star.
+
+star = Star(0.0,0.0,0.0,M_sun,R_sun,4000.0)
+star.set_blackbody_spectrum(dust.nu)
+
+# Set up the grid.
+
+nx = 10
+ny = 10
+nz = 10
+
+x = (arange(nx)-(float(nx)-1)/2)*AU/1
+y = (arange(ny)-(float(ny)-1)/2)*AU/1
+z = (arange(nz)-(float(nz)-1)/2)*AU/1
+
+density = zeros((nx-1,ny-1,nz-1)) + 1.0e-16
+
+model.set_cartesian_grid(x,y,z)
+model.grid.add_density(density, dust)
+model.grid.add_source(star)
+
+# Set the parameters for the run.
+
+model.params.nphot = 1000000
+model.params.bw = True
+model.params.scattering = False
+model.params.verbose = False
+model.params.use_mrw = False
+model.params.mrw_gamma = 2
+
+# Run the thermal simulation.
+
+t1 = time()
+model.run_thermal_mc()
+t2 = time()
+print(t2-t1)
+
+# Run the images.
+
+model.camera.nx = 256
+model.camera.ny = 256
+model.camera.pixel_size = AU/10
+model.camera.lam = array([1300.])
+
+image = model.run_image(incl=pi/4, pa=pi/4, dpc=1.)
+
+# Run the spectra.
+
+model.camera.pixel_size = 10*AU/100
+model.camera.lam = logspace(-1,4,200)
+
+spectrum = model.run_spectrum(incl=0, pa=0, dpc=1.)
+
+################################################################################
+#
+# Make some plots.
+#
+################################################################################
+
+# Plot the temperature structure.
+
+for i in range(9):
+    fig, ax = plt.subplots(nrows=1, ncols=3)
+
+    vmin = min(m.grid.temperature[0].min(), model.grid.temperature[0].min())
+    vmax = min(m.grid.temperature[0].max(), model.grid.temperature[0].max())
+
+    diff = (m.grid.temperature[0][:,:,i] - model.grid.temperature[0][:,:,i]) / \
+            m.grid.temperature[0][:,:,i] * 100
+
+    im1 = ax[0].imshow(m.grid.temperature[0][:,:,i], origin="lower", \
+            interpolation="nearest", vmin=vmin, vmax=vmax)
+
+    im2 = ax[1].imshow(model.grid.temperature[0][:,:,i], origin="lower",\
+            interpolation="nearest", vmin=vmin, vmax=vmax)
+
+    im3 = ax[2].imshow(diff, origin="lower", interpolation="nearest")
+
+    fig.colorbar(im1, ax=ax[1], fraction=0.046)
+    fig.colorbar(im3, ax=ax[2], fraction=0.046)
+
+    fig.subplots_adjust(left=0.1, right=0.95, wspace=0.25)
+    fig.set_size_inches((9,3))
+
+    plt.show()
+
+# Plot the images.
+
+fig, ax = plt.subplots(nrows=1, ncols=2)
+
+ax[0].imshow(m.images["image"].image[:,:,0,0], origin="lower", \
+        interpolation="none")
+
+ax[1].imshow(image.intensity[:,:,0], origin="lower", interpolation="none")
+
+fig.subplots_adjust(left=0.1, right=0.95, wspace=0.25)
+fig.set_size_inches((6,3))
+
+plt.show()
+
+# Plot the spectra.
+
+fig, ax = plt.subplots(nrows=1, ncols=1)
+
+ax.loglog(m.spectra["SED"].wave, m.spectra["SED"].flux)
+ax.loglog(spectrum.lam, spectrum.intensity)
+
 plt.show()
